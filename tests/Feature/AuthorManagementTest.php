c80b2b33113d62c6994e42000d9be35637f7221f
mohammedbakr <bakr.engineer@gmail.com>
2020-04-08 12:28:24 +0200

    create basic tests for Author model

---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Author;
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_author_cane_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/author', [
+            'name' => 'Mohamed',
+            'dob' => '04/08/2020',
+        ]);
+
+        $author =  Author::all();
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('2020/08/04', $author->first()->dob->format('Y/d/m'));
+    }
+}

21c35e32f3f716412228272b5db439f65b62dd8a
mohammedbakr <bakr.engineer@gmail.com>
2020-04-09 06:11:45 +0200

    make Reservation Model and its migration and checkin & checkout test

--- app/Book.php
+++ app/Book.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
@@ -12,5 +13,41 @@ class Book extends Model
     public function path()
     {
         return '/books/' . $this->id . '-' . Str::slug($this->title);
+    }
+
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->wherenull('checked_in_at')
+            ->first();
+
+        if(is_null($reservation)){
+            throw new Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now(),
+        ]);
+    }
+
+    public function setAuthorIdattribute($author)
+    {
+        $this->attributes['author_id'] = (Author::firstOrCreate([
+            'name' => $author,
+        ]))->id;
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
